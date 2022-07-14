0d866ef0571c80977bde28ff5ef173cbdd310d4c
Max <max.t.c.law@gmail.com>
2022-07-14 17:31:14 -0400

    created dropdown for all NA cities

--- script.js
+++ script.js
@@ -8,16 +8,49 @@ console.log(qualityApp.dropdownElement);
 qualityApp.formElement = document.querySelector('form');
 console.log(qualityApp.formElement);
 
+// Populate the dropdown list with cities in North America
+qualityApp.createDropdown = () => {
+    const url = new URL(`https://api.teleport.org/api/continents/geonames%3ANA/urban_areas/`);
+
+    fetch(url)
+    .then(function (response) {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error(response.statusText);
+        }
+    })
+    .then(function (cityListResult) {
+        const cityListArray = cityListResult._links["ua:items"];
+        qualityApp.populateDropdown(cityListArray);
+    })
+    .catch(function (error) {
+        alert(error.message);
+    })
+}
+
+// Appends options to the dropdown
+qualityApp.populateDropdown = (cityListArray) => {
+    cityListArray.forEach(function (city) {
+        const cityOption = document.createElement('option');
+        cityOption.value = city.href;
+        cityOption.textContent = city.name;
+        qualityApp.dropdownElement.append(cityOption);
+    })
+}
+
+// Get the user's city selection
 qualityApp.getCity = () => {
-    //listen for user submission
+    // Listen for user submission
     qualityApp.formElement.addEventListener('submit', function(e) {
         e.preventDefault();
 
-        //store the selected city in a variable 
-        const selectedCityValue = qualityApp.dropdownElement.value;
+        // Store the selected city API url
+        const selectedCityName = qualityApp.dropdownElement.selectedOptions[0].innerText;
+        const selectedCityHref = qualityApp.dropdownElement.value;
 
-        //make an API call to get the selected city data
-        const url = new URL(`https://api.teleport.org/api/urban_areas/slug:${selectedCityValue}/scores/`);
+        // Make an API call to get the selected city data endpoint
+        const url = new URL(`${selectedCityHref}scores/`);
 
         fetch(url)
         .then(function (response) {
@@ -28,8 +61,9 @@ qualityApp.getCity = () => {
             }
         })
         .then(function (cityData) {
-            console.log(cityData);
-            qualityApp.displayScores(selectedCityValue, cityData)
+            const cityScoresArray = cityData.categories;
+            qualityApp.displayScores(selectedCityName, cityScoresArray)
+            console.log(selectedCityName);
         })
         .catch(function(error) {
             alert(error.message);
@@ -39,18 +73,18 @@ qualityApp.getCity = () => {
     
 }
 
-qualityApp.displayScores = (cityName, dataObject) => {
+qualityApp.displayScores = (cityName, cityScores) => {
+    
+    // Display city name
+    const cityNameElement = document.querySelector('#cityName');
+    cityNameElement.innerText = cityName;
+    
+    // Clear the list first
     const cityScoresElement = document.querySelector('#cityScores');
     cityScoresElement.innerHTML = '';
 
-    const cityNameElement = document.querySelector('#cityName');
-    const selectedCityName = qualityApp.dropdownElement.selectedOptions[0].innerText;
-
-    // Display the city name
-    cityNameElement.innerText = selectedCityName;
-
     // Create and append the score list items
-    dataObject.categories.forEach(function (category) {
+    cityScores.forEach(function (category) {
         const listElement = document.createElement('li');
         listElement.innerHTML = `<h3>${category.name}: </h3><p class="score">${category.score_out_of_10.toFixed(1)}/10</p>`
 
@@ -61,6 +95,7 @@ qualityApp.displayScores = (cityName, dataObject) => {
 
 
 qualityApp.init = () => {
+    qualityApp.createDropdown();
     qualityApp.getCity();
 }
 
